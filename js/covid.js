0520c0399eb717f857ef821f99fa75143fdbc593
Choy <hchoy@partners.org>
2020-12-06 21:23:14 -0500

    added covid dashboard

--- js/covid.js
+++ js/covid.js
@@ -1,17 +1,82 @@
 /*jshint esversion: 6 */
 
-const form = document.getElementById("form");
-const inputValue = document.getElementById("inputValue");
+
 const display = document.getElementById("display");
 const displayUSA = document.getElementById("display-USA");
 
-window.addEventListener("load", getWorld);
+getUSATotals();
+getUSA();
+getWorld();
+
+function getUSATotals(e){
+
+    const request = new XMLHttpRequest();
+    request.withCredentials = false;
+    request.open('GET', 'https://api.covidtracking.com/v1/us/current.json', true);
+
+    request.onload = function () {
+        if (request.status === 200) {
+            let dataUSA = JSON.parse(request.responseText);
+            window.console.log(dataUSA);
+
+            document.getElementById("usa-cases").innerHTML = `Total Cases<br>
+                                                                       ${dataUSA[0].positive.toLocaleString()}<br>
+                                                                       +${dataUSA[0].positiveIncrease.toLocaleString()}`;
+            document.getElementById("usa-deaths").innerHTML = `Total Deaths<br>
+                                                                        <div id="deaths">${dataUSA[0].death.toLocaleString()}<br>
+                                                                        +${dataUSA[0].deathIncrease.toLocaleString()}</div>`;
+            document.getElementById("usa-recovered").innerHTML = `Total Recovered<br>
+                                                                           <div id="recovered">${dataUSA[0].recovered.toLocaleString()}<br>
+                                                                           ---</div>`;
+        }
+
+    };
+
+    request.send();
+
+}
+
+function getUSA(e){
+
+    const request = new XMLHttpRequest();
+    request.withCredentials = false;
+    request.open('GET', 'https://api.covidtracking.com/v1/states/current.json', true);
+
+    request.onload = function () {
+        if (request.status === 200) {
+            let dataUSA = JSON.parse(request.responseText);
+            window.console.log(dataUSA);
+
+            document.getElementById("MA-updated").innerHTML = `Last Updated (for MA): ${dataUSA[21].dateModified}`;
+
+            let htmlStringUSA = '';
+            for (let i=0; i < dataUSA.length; i++) {
+                htmlStringUSA += `<tr><td class='row-name'>${states[dataUSA[i].state]}</td>
+                                      <td>${dataUSA[i].positiveIncrease.toLocaleString()}</td>
+                                      <td>${dataUSA[i].deathIncrease.toLocaleString()}</td>
+                                      <td>${dataUSA[i].positive.toLocaleString()}</td>
+                                      <td>${dataUSA[i].death.toLocaleString()}</td>
+                                 </tr>`;
+                }
+
+            displayUSA.insertAdjacentHTML("afterbegin",htmlStringUSA);
+
+            }
+
+        else {
+            displayUSA.innerHTML = "Data not found! Please try again later";
+        }
+    };
+
+    request.send();
+
+}
 
 
 function getWorld(e){
 
     const request = new XMLHttpRequest();
-    request.withCredentials = true;
+    request.withCredentials = false;
     request.open('GET', 'https://api.covid19api.com/summary', true);
 
     request.onload = function () {
@@ -19,26 +84,102 @@ function getWorld(e){
             let data = JSON.parse(request.responseText);
             window.console.log(data);
 
+            document.getElementById("global-cases").innerHTML = `Total Cases<br>
+                                                                          ${data.Global.TotalConfirmed.toLocaleString()}<br>
+                                                                          +${data.Global.NewConfirmed.toLocaleString()}`;
+            document.getElementById("global-deaths").innerHTML = `Total Deaths<br>
+                                                                           <div id="deaths">${data.Global.TotalDeaths.toLocaleString()}<br>
+                                                                           +${data.Global.NewDeaths.toLocaleString()}</div>`;
+            document.getElementById("global-recovered").innerHTML = `Total Recovered<br>
+                                                                              <div id="recovered">${data.Global.TotalRecovered.toLocaleString()}<br>
+                                                                              +${data.Global.NewRecovered.toLocaleString()}</div>`;
+
+
+            document.getElementById("global-updated").innerHTML = `Last Updated: ${data.Date}`;
+
+
             let htmlString = '';
             for (let i=0; i < data.Countries.length; i++) {
-                if (i % 2 === 0) {
-                    htmlString += "<tr class='even'><td class='row-name'>"+data.Countries[i].Country+"</td>" + "<td>"+data.Countries[i].NewConfirmed.toLocaleString()+"</td>"+
-                        "<td>"+data.Countries[i].NewDeaths.toLocaleString()+"</td>" + "<td>" + data.Countries[i].TotalConfirmed.toLocaleString() + "</td>" + "<td>"+ data.Countries[i].TotalDeaths.toLocaleString() + "</td></tr>";
-                }
-                else {
-                    htmlString += "<tr><td class='row-name'>"+data.Countries[i].Country+"</td>" + "<td>"+data.Countries[i].NewConfirmed.toLocaleString()+"</td>"+
-                        "<td>"+data.Countries[i].NewDeaths.toLocaleString()+"</td>" +" <td>" + data.Countries[i].TotalConfirmed.toLocaleString() + "</td>" + "<td>"+ data.Countries[i].TotalDeaths.toLocaleString() + "</td></tr>";
-                }
+                htmlString += `<tr><td class='row-name'>${data.Countries[i].Country}</td>
+                                    <td>${data.Countries[i].NewConfirmed.toLocaleString()}</td>
+                                    <td>${data.Countries[i].NewDeaths.toLocaleString()}</td>
+                                    <td>${data.Countries[i].TotalConfirmed.toLocaleString()}</td>
+                                    <td>${data.Countries[i].TotalDeaths.toLocaleString()}</td>
+                                </tr>`;
             }
-
             display.insertAdjacentHTML("afterbegin",htmlString);
 
-
         }
-        if (request.status === 404) {
+        else {
             display.innerHTML = "Data not found! Please try again later";
         }
     };
 
     request.send();
 }
+
+
+const states = {
+    AZ: 'Arizona',
+    AL: 'Alabama',
+    AK: 'Alaska',
+    AR: 'Arkansas',
+    CA: 'California',
+    CO: 'Colorado',
+    CT: 'Connecticut',
+    DC: 'District of Columbia',
+    DE: 'Delaware',
+    FL: 'Florida',
+    GA: 'Georgia',
+    HI: 'Hawaii',
+    ID: 'Idaho',
+    IL: 'Illinois',
+    IN: 'Indiana',
+    IA: 'Iowa',
+    KS: 'Kansas',
+    KY: 'Kentucky',
+    LA: 'Louisiana',
+    ME: 'Maine',
+    MD: 'Maryland',
+    MA: 'Massachusetts',
+    MI: 'Michigan',
+    MN: 'Minnesota',
+    MS: 'Mississippi',
+    MO: 'Missouri',
+    MT: 'Montana',
+    NE: 'Nebraska',
+    NV: 'Nevada',
+    NH: 'New Hampshire',
+    NJ: 'New Jersey',
+    NM: 'New Mexico',
+    NY: 'New York',
+    NC: 'North Carolina',
+    ND: 'North Dakota',
+    OH: 'Ohio',
+    OK: 'Oklahoma',
+    OR: 'Oregon',
+    PA: 'Pennsylvania',
+    RI: 'Rhode Island',
+    SC: 'South Carolina',
+    SD: 'South Dakota',
+    TN: 'Tennessee',
+    TX: 'Texas',
+    UT: 'Utah',
+    VT: 'Vermont',
+    VA: 'Virginia',
+    WA: 'Washington',
+    WV: 'West Virginia',
+    WI: 'Wisconsin',
+    WY: 'Wyoming',
+    AS: "American Samoa",
+    GU: "Guam",
+    MP: "Northern Mariana Islands",
+    PR: "Puerto Rico",
+    VI: "U.S. Virgin Islands",
+    UM: "U.S. Minor Outlying Islands",
+};
+
+
+
+
+
--- js/weather.js
+++ js/weather.js
@@ -4,24 +4,47 @@ const form = document.getElementById("form");
 const inputValue = document.getElementById("inputValue");
 const display = document.getElementById("display");
 
-document.getElementById("form").addEventListener('submit', getWeather);
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getWeather);
+    } else {
+        window.console.log("Browser does not support geolocation!");
+    }
+}
 
-function getWeather(e){
-    e.preventDefault();
+getLocation();
+
+function getWeather(position){
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
     const request = new XMLHttpRequest();
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&mode=json&appid=70ef88575b961360bd130eeab051f901', true);
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ long +'&units=imperial&mode=json&appid=70ef88575b961360bd130eeab051f901', true);
 
     request.onload = function () {
-        if (request.status === 200) {
+        if (request.status === 200 && navigator.geolocation) {
+
             const data = JSON.parse(request.responseText);
+
+            display.innerHTML = `<h3>Current conditions in your area: ${data.name}</h3> <br>
+                                 <img id='weather' src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="conditions"> <br>
+                                 Temperature: ${data.main.temp.toFixed(0)}&deg <br>
+                                 Feels like: ${data.main.feels_like.toFixed(0)}&deg <br>
+                                 Conditions: ${data.weather[0].description} <br>
+                                 `;
             window.console.log(data);
-            display.innerHTML = data.main.temp;
         }
         if (request.status === 404) {
-            display.innerHTML = "City not found! Please try again.";
+            display.innerHTML = "Weather data unavailable! Please try again later.";
         }
+
     };
 
     request.send();
 }
+
+
+
+
+
+
